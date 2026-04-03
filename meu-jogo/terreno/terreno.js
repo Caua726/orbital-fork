fc8bb5845a2668c92f767975a3066b927ebd7d7b
miguelmarchisnow-maker <miguelmarchisnow-maker@users.noreply.github.com>
2026-04-03 16:04:09 -0300

    Terreno de 60 para 70

--- meu-jogo/terreno/terreno.js
+++ meu-jogo/terreno/terreno.js
@@ -7,7 +7,7 @@ export function criarTerreno(app) {
   const g = new Graphics();
   const w = app.screen.width;
   const h = app.screen.height;
-  const alturaCeus = h * 0.60;
+  const alturaCeus = h * 0.70;
 
   g.rect(0, 0, w, alturaCeus).fill({ color: 0x87ceeb });
   g.rect(0, alturaCeus, w, h - alturaCeus).fill({ color: 0x3d8c40 });
